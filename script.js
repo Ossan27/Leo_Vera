ef7304d7a384751659b4601a9dd8e355f93297eb
Ossan27 <rubossan@upv.edu.es>
2024-11-15 09:25:38 +0000

    Ahora cambia nombre cada vez que se hace click

--- script.js
+++ script.js
@@ -18,22 +18,12 @@ function removePetal() {
         daisy.removeChild(daisy.lastChild);
         petalCount--;
         document.getElementById('countdown').innerText = petalCount;
-        if (petalCount === 0) {
-            toggleName();
-        }
+        toggleName();
+        
     }
 }
 
 function toggleName() {
     currentName = currentName === 'Vera' ? 'Leo' : 'Vera';
     document.getElementById('name').innerText = currentName;
-    petalCount = 10;
-    document.getElementById('countdown').innerText = petalCount;
-    const daisy = document.getElementById('daisy');
-    for (let i = 0; i < petalCount; i++) {
-        const petal = document.createElement('div');
-        petal.className = 'petal';
-        petal.style.transform = `rotate(${i * (360 / petalCount)}deg) translateY(-100px)`;
-        daisy.appendChild(petal);
-    }
 }
